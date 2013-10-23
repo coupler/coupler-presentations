c89df1ca742504b858a9e34420fff1cc48fb7ccd
Jeremy Stephens <viking@pillageandplunder.net>
2013-10-22 22:00:31 -0500

    Add more slides

--- biostat-seminar-2013/examples/etl.R
+++ biostat-seminar-2013/examples/etl.R
@@ -1,3 +1,32 @@
-# extract data
-x <- read.csv('my-data.csv')
-y <- read.csv('their-data.csv')
+## extract
+x <- read.csv('my-data.csv',
+  as.is = TRUE)
+y <- read.csv('their-data.csv',
+  as.is = TRUE)
+
+## transform
+# split up name
+name.split <- strsplit(x$name, " ")
+x$firstname <- sapply(name.split,
+  function(v) v[1])
+x$lastname <- sapply(name.split,
+  function(v) v[2])
+
+# merge data
+dataset <- merge(x, y,
+  by = c("firstname", "lastname"))
+
+# select fields
+fields <- c("firstname", "lastname",
+  "dob", "height", "weight")
+dataset <- dataset[, fields]
+
+# calculate age
+now <- as.Date("2062-10-23")
+dob.date <- as.Date(dataset$dob)
+age.days <- as.numeric(
+  difftime(now, dob.date))
+dataset$age <- age.days %/% 365.25
+
+## load
+save(dataset, file="dataset.Rdata")
